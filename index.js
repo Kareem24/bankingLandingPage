4359c3c718596f6ec2885359a880c1a706df9202
kareem roqib <kareemolakunleh@gmail.com>
2022-10-11 23:35:59 +0100

    testing animation on scroll will vanilla javascript and css animation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelectorAll('.Btn');
 const navlink = document.querySelector('.navlink');
 const main = document.getElementById('main');
 hamburger.forEach((btn) => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', (e) => {
         navlink.classList.toggle('show__navlinks');
         main.classList.toggle('light-box');
     });
@@ -12,4 +12,32 @@ window.addEventListener('scroll', () => {
          navlink.classList.remove('show__navlinks');
         main.classList.remove('light-box');
     }
+  
+    const image = document.querySelectorAll('.img__col');
+    // console.log(image);
+    image.forEach((img)=>{
+        const imageHeight = img.getBoundingClientRect().top
+    if (pageYOffset > imageHeight  ){
+        img.classList.add('show__phone')
+        
+    }
+    else {
+        img.classList.remove('show__phone')
+        
+        };
+    })
+    const text = document.querySelectorAll('.text__col');
+     text.forEach((txt)=>{
+        const textHeight = txt.getBoundingClientRect().top
+    if (pageYOffset > textHeight ){
+        txt.classList.add('showtext')
+        
+    }
+    else {
+        txt.classList.remove('showtext')
+        
+        };
+    })
+   
+    // console.log(pageYOffset);
 });
